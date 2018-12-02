8ef223a9af74f8e9e317b080f9690f865cf770a1
aishabn <abn.226@gmail.com>
2018-12-02 13:19:32 +0300

    bootstrap_challenge

--- tourista/settings.py
+++ tourista/settings.py
@@ -130,4 +130,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-CRISPY_TEMPLATE_PACK = 'bootstrap3'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
